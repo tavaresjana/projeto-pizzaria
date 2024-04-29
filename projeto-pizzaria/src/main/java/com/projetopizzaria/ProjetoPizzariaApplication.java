ed4ceb60876029b1a97e86410c12bbe65936feb0
Janaina Tavares <78567230+tavaresjana@users.noreply.github.com>
2024-04-29 20:55:10 -0300

    first commit

---
+++ projeto-pizzaria/src/main/java/com/projetopizzaria/ProjetoPizzariaApplication.java
@@ -0,0 +1,13 @@
+package com.projetopizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPizzariaApplication.class, args);
+	}
+
+}
---
+++ projeto-pizzaria/src/test/java/com/projetopizzaria/ProjetoPizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetopizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
