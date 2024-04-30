96ebc18e201d4f115d11e65d3685cfa0925c0b42
Janaina Tavares <78567230+tavaresjana@users.noreply.github.com>
2024-04-30 13:38:24 -0300

    feat: crud entidade cliente

--- projeto-pizzaria/src/main/java/com/projetopizzaria/controllers/ClienteController.java
+++ projeto-pizzaria/src/main/java/com/projetopizzaria/controllers/ClienteController.java
@@ -5,7 +5,11 @@ import com.projetopizzaria.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +25,24 @@ public class ClienteController {
     public ResponseEntity<ClienteDto> cadastrarCliente(@RequestBody ClienteDto clienteDto){
         clienteService.cadastrarCliente(clienteDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClienteDto> buscarClientePorId(@PathVariable Long id){
+        ClienteDto clienteDto = clienteService.buscarClientePorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(clienteDto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClienteDto> atualizarCliente(@PathVariable Long id, @RequestBody ClienteDto clienteDto){
+        clienteService.atualizarCliente(clienteDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deletarCliente(@PathVariable Long id){
+        clienteService.deletarCliente(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- projeto-pizzaria/src/main/java/com/projetopizzaria/service/ClienteService.java
+++ projeto-pizzaria/src/main/java/com/projetopizzaria/service/ClienteService.java
@@ -2,9 +2,12 @@ package com.projetopizzaria.service;
 
 import com.projetopizzaria.dto.ClienteDto;
 import com.projetopizzaria.mappers.ClienteMapper;
+import com.projetopizzaria.models.Cliente;
 import com.projetopizzaria.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -17,5 +20,29 @@ public class ClienteService {
 
     public ClienteDto cadastrarCliente(ClienteDto clienteDto){
         return clienteMapper.entidadeParaDto(clienteRepository.save(clienteMapper.dtoParaEntidade(clienteDto)));
+    }
+
+    public ClienteDto buscarClientePorId(Long id){
+        return clienteMapper.entidadeParaDtoOp(clienteRepository.findById(id));
+    }
+
+    public ClienteDto atualizarCliente(ClienteDto clienteDto){
+        Optional<Cliente> optionalCliente = clienteRepository.findById(clienteDto.getIdCliente());
+
+        Cliente clienteEditado = optionalCliente.get();
+
+        clienteEditado.setNome(clienteDto.getNome());
+        clienteEditado.setTelefone(clienteDto.getTelefone());
+        clienteEditado.setEndereco(clienteDto.getEndereco());
+        clienteEditado.setLogin(clienteDto.getLogin());
+        clienteEditado.setSenha(clienteDto.getSenha());
+
+        clienteRepository.save(clienteEditado);
+
+        return clienteMapper.entidadeParaDto(clienteEditado);
+    }
+
+    public void deletarCliente(Long id){
+        clienteRepository.deleteById(id);
     }
 }
